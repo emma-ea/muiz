3c5269180eaeee91ccec4cdeb34ea23ff655ec55
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2022-08-26 04:01:56 +0000

    song model, initial room db config

---
+++ app/src/main/java/com/emma_ea/muiz/model/data/MusicDatabase.kt
@@ -0,0 +1,24 @@
+package com.emma_ea.muiz.model.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.emma_ea.muiz.model.Song
+
+@Database(entities = [Song::class], version = 1, exportSchema = false)
+abstract class MusicDatabase : RoomDatabase() {
+
+    abstract fun musicDAO : MusicDao
+
+    companion object {
+        private var database: MusicDatabase? = null
+        fun getDatabase(context: Context): MusicDatabase {
+            database?:kotlin.run {
+                database = Room.databaseBuilder(context, MusicDatabase::class.java, "music_database")
+                    .build()
+            }
+            return database!!
+        }
+    }
+}
