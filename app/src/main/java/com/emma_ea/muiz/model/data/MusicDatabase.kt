87d1e92b44be9c0750de39334c648a3e3b8b12e1
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2022-08-27 02:54:11 +0000

    prepopulate db in onCreate

--- app/src/main/java/com/emma_ea/muiz/model/data/MusicDatabase.kt
+++ app/src/main/java/com/emma_ea/muiz/model/data/MusicDatabase.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.emma_ea.muiz.model.Song
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Database(entities = [Song::class], version = 1, exportSchema = false)
 abstract class MusicDatabase : RoomDatabase() {
@@ -13,12 +16,35 @@ abstract class MusicDatabase : RoomDatabase() {
 
     companion object {
         private var database: MusicDatabase? = null
-        fun getDatabase(context: Context): MusicDatabase {
+        fun getDatabase(context: Context, scope: CoroutineScope): MusicDatabase {
             database?:kotlin.run {
                 database = Room.databaseBuilder(context, MusicDatabase::class.java, "music_database")
+                    .addCallback(roomCallback(scope))
                     .build()
             }
             return database!!
+        }
+
+        private fun roomCallback(scope: CoroutineScope) = object : RoomDatabase.Callback() {
+            override fun onCreate(db: SupportSQLiteDatabase) {
+                super.onCreate(db)
+                database?.let {
+                    scope.launch {
+                        it.musicDAO().insert(
+                            Song(
+                                1,
+                                1001,
+                                "Guitar Solo",
+                                "Guitarist",
+                                "Greatest Hits",
+                                "22",
+                                "content://1011010",
+                                "2022"
+                            )
+                        )
+                    }
+                }
+            }
         }
     }
 }
